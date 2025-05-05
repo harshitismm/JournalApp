74d38bb491da4fe7ec45ca1d3e8ff09a8d7c198f
harshit <harahitg@gmail.com>
2025-05-05 17:56:21 +0530

    Antmatcher fix

--- src/main/java/net/engineeringdigest/journalApp/config/SecurityConfig.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/journal/**,/user/**").authenticated()
+                .antMatchers("/journal/**","/user/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
